02fa4047da761afb286434c51aaffcac76af5242
Myriam Chevalier <chevalier.myriam@hotmail.com>
2021-07-30 10:13:14 -0500

    added import functions to access data from other modules, stored them into variables

---
+++ scripts/Cars-R-Us.js
@@ -0,0 +1,11 @@
+import { InteriorMaterials } from "./Interiors.js";
+import { PaintColorChoices } from "./PaintColors.js";
+import { TechnologyPackages } from "./TechnologyPackages.js";
+import { WheelsOptions } from "./WheelsOptions.js";
+
+
+
+const paintColorChoices = PaintColorChoices()
+const technologyPackages = TechnologyPackages()
+const interiorMaterials = InteriorMaterials()
+const wheelsOptions = WheelsOptions()
